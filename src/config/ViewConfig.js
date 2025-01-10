165753cc03a4dc900da675bb913562223a3ecef5
Ripaldiansyah <ripaldiansyah13@gmail.com>
2025-01-10 16:51:26 +0700

    .

--- src/config/ViewConfig.js
+++ src/config/ViewConfig.js
@@ -1,10 +1,8 @@
 module.exports = (app, express, path, hbs) => {
   app.set("view engine", "hbs");
   app.set("views", path.join(__dirname + "../../views"));
-  const rootDir = path.join(__dirname, "..", "..");
-
-  app.use("/assets", express.static(path.join(rootDir, "assets")));
-  app.use("/images", express.static(path.join(rootDir, "uploads")));
+  app.set("assets", path.join(__dirname + "../../assets"));
+  app.set("images", path.join(__dirname + "../../uploads"));
 
   // app.use("/assets", express.static(path.join(__dirname + "../../assets")));
   // app.use("/images", express.static(path.join(__dirname + "../../uploads")));
