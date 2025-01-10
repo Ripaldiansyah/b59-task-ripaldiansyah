206a8fc9b931c5992610d2febacfdbe3f31799c1
Ripaldiansyah <ripaldiansyah13@gmail.com>
2025-01-10 16:13:21 +0700

    .

--- src/config/ViewConfig.js
+++ src/config/ViewConfig.js
@@ -2,8 +2,11 @@ module.exports = (app, express, path, hbs) => {
   app.set("view engine", "hbs");
   app.set("views", path.join(__dirname, "../../src/views"));
 
-  app.use("/assets", express.static(path.join(__dirname, "../../src/assets")));
-  app.use("/images", express.static(path.join(__dirname, "../../src/uploads")));
+  app.use("/assets", express.static(path.join(__dirname + "../../src/assets")));
+  app.use(
+    "/images",
+    express.static(path.join(__dirname + "../../src/uploads"))
+  );
   hbs.registerPartials(
     path.join(__dirname, "../../src/views/partials"),
     (err) => {
