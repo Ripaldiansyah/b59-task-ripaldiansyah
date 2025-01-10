e44e7ffd777def746b71aed50f7ff743a1dd1766
Ripaldiansyah <ripaldiansyah13@gmail.com>
2025-01-10 16:46:35 +0700

    .

--- src/config/ViewConfig.js
+++ src/config/ViewConfig.js
@@ -1,9 +1,13 @@
 module.exports = (app, express, path, hbs) => {
   app.set("view engine", "hbs");
   app.set("views", path.join(__dirname + "../../views"));
+  const rootDir = path.join(__dirname, "..", "..");
 
-  app.use("/assets", express.static(path.join(__dirname + "../../assets")));
-  app.use("/images", express.static(path.join(__dirname + "../../uploads")));
+  app.use("/assets", express.static(path.join(rootDir, "assets")));
+  app.use("/images", express.static(path.join(rootDir, "uploads")));
+
+  // app.use("/assets", express.static(path.join(__dirname + "../../assets")));
+  // app.use("/images", express.static(path.join(__dirname + "../../uploads")));
   hbs.registerPartials(path.join(__dirname + "../../views/partials"), (err) => {
     if (err) {
       console.error("Error registering partials:", err);
