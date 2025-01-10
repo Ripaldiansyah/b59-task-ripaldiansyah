6129f684f1b5d9502ab46363d589ed980c90e415
Ripaldiansyah <ripaldiansyah13@gmail.com>
2025-01-10 16:18:15 +0700

    .

--- src/config/ViewConfig.js
+++ src/config/ViewConfig.js
@@ -1,18 +1,12 @@
 module.exports = (app, express, path, hbs) => {
   app.set("view engine", "hbs");
-  app.set("views", path.join(__dirname + "../../src/views"));
+  app.set("views", path.join(__dirname + "../../views"));
 
-  app.use("/assets", express.static(path.join(__dirname + "../../src/assets")));
-  app.use(
-    "/images",
-    express.static(path.join(__dirname + "../../src/uploads"))
-  );
-  hbs.registerPartials(
-    path.join(__dirname + "../../src/views/partials"),
-    (err) => {
-      if (err) {
-        console.error("Error registering partials:", err);
-      }
+  app.use("/assets", express.static(path.join(__dirname + "../../assets")));
+  app.use("/images", express.static(path.join(__dirname + "../../uploads")));
+  hbs.registerPartials(path.join(__dirname + "../../views/partials"), (err) => {
+    if (err) {
+      console.error("Error registering partials:", err);
     }
-  );
+  });
 };
--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,4 @@
 "use strict";
-const pg = require("pg");
 const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
