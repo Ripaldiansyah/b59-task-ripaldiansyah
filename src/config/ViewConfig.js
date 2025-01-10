2dadff62e545c542156b9c937ba79b0706e550d2
Ripaldiansyah <ripaldiansyah13@gmail.com>
2025-01-10 15:52:36 +0700

    .

--- src/config/ViewConfig.js
+++ src/config/ViewConfig.js
@@ -1,17 +1,11 @@
 module.exports = (app, express, path, hbs) => {
   app.set("view engine", "hbs");
+  app.set("views", path.join(__dirname, "../../src/views"));
 
-  app.set("views", path.join(process.cwd(), "src", "views")); // Path menuju src/views
-
-  // Menyajikan file statis (assets dan uploads)
-  app.use("/assets", express.static(path.join(process.cwd(), "src", "assets")));
-  app.use(
-    "/images",
-    express.static(path.join(process.cwd(), "src", "uploads"))
-  );
-
+  app.use("/assets", express.static(path.join(__dirname, "../../src/assets")));
+  app.use("/images", express.static(path.join(__dirname, "../../src/uploads")));
   hbs.registerPartials(
-    path.join(process.cwd(), "src", "views", "partials"),
+    path.join(__dirname, "../../src/views/partials"),
     (err) => {
       if (err) {
         console.error("Error registering partials:", err);
